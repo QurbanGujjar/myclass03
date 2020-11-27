4e87f49d775c1922bf2f284929786941311de0f1
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-11-27 12:44:13 +0500

    Create Room component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
+import Room from './components/Room'
 
 function App() {
   return (
     <div className="App">
       <Hello name ='Qurban Ali'/>
+      <Room/>
       
     </div>
   );
