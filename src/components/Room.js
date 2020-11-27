b500d4652f99b72d33c030c9b555acc07e8bf7da
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-11-27 13:26:35 +0500

    update Room
    loggle button and counter Button

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React,{ useState}  from 'react';
 
 function Room() {
-    
+    let [isLit,setlit] = useState(false)
+    let [Counter,CounterFun] = useState(0)
+
+
+
+    function updateLit(){
+        console.log("button clicked")
+        setlit(!isLit)
+    }
+    function MyCounterF(){
+        CounterFun(++Counter)
+    }
+
         return (
             <div>
                 This is Room Component
+        <p>The Room  is {isLit? "lit":"Dark"}</p>
+        <button onClick={updateLit}>Toggle Light</button>
+        <p> Counter value {Counter}</p>
+        <button onClick={MyCounterF}>Click for Count</button>
+
             </div>
         );
     
