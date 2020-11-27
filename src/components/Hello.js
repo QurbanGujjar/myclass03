466c3dfc6cbcf3416c0213a904d3b175325682f5
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-11-27 12:09:01 +0500

    heading and list

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -5,6 +5,22 @@ class Hello extends Component {
         return (
             <div>
                 <p>Hello World From <strong> { this.props.name} </strong></p>
+            <h1>Unordered List of Items</h1>
+            <ul>
+                <li>Apple</li>
+                <li>Orange</li>
+                <li>Banana</li>
+                <li>Mango</li>
+                <li>Gawa</li>
+            </ul>
+            <h1>Ordered List of Items</h1>
+           <ol>
+                <li>Apple</li>
+                <li>Orange</li>
+                <li>Banana</li>
+                <li>Mango</li>
+                <li>Gawa</li>
+            </ol>
             </div>
         )
     }
