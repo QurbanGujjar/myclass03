c690cf5c085ed3ea5ea84d0c1acd75e13228bb16
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-11-29 20:04:29 +0500

    add img

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
+
+
 function MediaCard(props){
 
         return (
             <div>
                 <h1>my name  is {props.name} </h1>
                 <h1>this is my tital {props.heroname} </h1>
-                <img src={require('media/saturday.jpeg')} />
+               <img src="/mediafile/saturday.jpeg" alt="abc"/>
 
 
             </div>
