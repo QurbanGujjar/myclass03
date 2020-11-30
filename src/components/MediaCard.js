4e9ea7be1604b66c608236b0f1bd6bcb62d95384
Qurban <qurbanali4747@gmail.com>
2020-11-30 20:13:29 +0500

    update media

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -5,9 +5,10 @@ function MediaCard(props){
 
         return (
             <div>
+                <h1>Hello </h1>
                 <h1>my name  is {props.name} </h1>
                 <h1>this is my tital {props.heroname} </h1>
-               <img src="/mediafile/saturday.jpeg" alt="abc"/>
+               
 
 
             </div>
